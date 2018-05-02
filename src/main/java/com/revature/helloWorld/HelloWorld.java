6b30235937bbff67d24efb7c3d5fa5f2732dfc20
mitch-harrison <38013934+mitch-harrison@users.noreply.github.com>
2018-05-02 15:52:15 -0400

    Add files via upload

--- src/main/java/com/revature/helloWorld/HelloWorld.java
+++ src/main/java/com/revature/helloWorld/HelloWorld.java
@@ -2,6 +2,6 @@ package com.revature.helloWorld;
 
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("HelloWorld");
+		System.out.println("HelloWorld, goodbye world");
 	}
 }
